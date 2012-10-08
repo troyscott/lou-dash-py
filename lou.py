e9bffa2b26f080e7d66ee43174998e9be8687fad
Troy Scott <troys2005@gmail.com>
2012-10-08 02:11:37 -0700

    created new session class and some test scripts

--- lou.py
+++ lou.py
@@ -5,29 +5,14 @@ from urllib import urlencode
 import json
 
 import loumodels
-domain = "www.lordofultima.com"
-url_login = "https://%s/en/user/login?destination=login" % (domain)
-url_start_game = "https://%s/en/game" % (domain)
-url_main = "http://prodgame05.lordofultima.com/197/"
-url_ajax = "%sPresentation/Service.svc/ajaxEndpoint/" % (url_main)
-url_server_info = "%sGetServerInfo" % (url_ajax)
+from lousession import Session
 
-user_mail = ""
-user_password = ""
+
+#url_ajax = "%sPresentation/Service.svc/ajaxEndpoint/" % (url_main)
+#url_server_info = "%sGetServerInfo" % (url_ajax)
+
 
 content_type = {'content-type':'application/json'}
-
-
-def login():
-    h = httplib2.Http()
-    data = dict(mail=user_mail, password=user_password)
-    response, content  = h.request(url_login, 'GET')
-    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Cookie': response['set-cookie']}
-    response, content  = h.request(url_login,'POST', headers=headers, body=urlencode(data))
-    headers = {'Cookie': response['set-cookie'] }
-    response, content  = h.request(url_start_game, 'GET', headers=headers)
-    return content 
-
 
 def get_server_info():
     h = httplib2.Http()
@@ -38,7 +23,17 @@ def get_server_info():
 
 
 if __name__ == "__main__":
-    print 'Test login'
-    print login()
-    #print get_server_info()
+    print 'Create Session'
+    #u = raw_input('user:')
+    #p = raw_input('password:')
+    u = ''
+    p = ''
+    s = Session(u, p)
+    print 'Property:'
+    print s.get_session_id()
+    #print 'Method:'
+    #print s.get_game_url()
+
+
+
 
--- lousession.py
+++ lousession.py
@@ -1,35 +1,60 @@
 #!/usr/bin/env python
 
-import httlib
+from subprocess import Popen, PIPE
+import httplib2
 from urllib import urlencode
-
+import re
 
 _DOMAIN = "www.lordofultima.com"
 _URL_LOGIN = "https://%s/en/user/login" % _DOMAIN
 _URL_HIDDEN_SESSION_ID = "https://%s/en/game" % _DOMAIN
 
 
-
 class Session():
+
 
     def __init__(self, mail, password):
         client = httplib2.Http()
         response, content = client.request(_URL_LOGIN,'GET')
-        headers = {'Content-type': 'applicationi/x-www-form-urlencoded',
+        headers = {'Content-type': 'application/x-www-form-urlencoded',
                 'Cookie': response['set-cookie']}
         body = dict(mail=mail, password=password)
         response, content = client.request(_URL_LOGIN, 'POST', headers=headers,
                 body=urlencode(body))
         headers = {'Cookie': response['set-cookie']}
-        response, content = client.request(URL_HIDDEN_SESSION_ID, 'GET'
-                headers=headers
-        _session = {}
-        _session = parse_session_data(content,client)
-  
+        response, content = client.request(_URL_HIDDEN_SESSION_ID, 'GET',
+                headers=headers)
+        self.session_data = content
 
-    def parse_session_data(self, contenti, client):
-        
-        
-        
-        
+    def get_session_id(self):
+        find_line = 'name="sessionId" id="sessionId"'
+        re_find_value = r".*name=\"sessionId\" id=\"sessionId\" value=\"(.*)\".*"
+        return self.parse_session_data(find_line, re_find_value)
+       
+
+    def get_game_url(self):
+        find_line = 'action="http://prodgame'
+        re_find_value = r"s/.*=\"\(.*\)\/.*\">/\1/"
+        return self.parse_session_data(find_line, re_find_value)
+    
+    def get_world_id(self):
+        find_line = 'value="World ' 
+        re_find_value = r"s/.*=\"\(.*\)\".*>/\1/"
+        return self.parse_session_data(find_line, re_find_value)
+    
+    def parse_session_data(self, find_line, re_find_value ):
+        data = self.session_data
+        p1 = Popen(["echo", data], stdout=PIPE)
+        p2 = Popen(["grep", find_line], stdin=p1.stdout, stdout=PIPE)
+        p1.stdout.close()
+        line = p2.communicate()[0]
+        print line
+        pattern = re.compile(re_find_value)
+        result = pattern.match(line)
+        return result.group(1)
+    
+
+
+    
+            
 
