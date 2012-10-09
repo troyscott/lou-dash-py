c5b26610f47031889b9aa97b7f427a656b285f81
Troy Scott <troys2005@gmail.com>
2012-10-08 20:53:24 -0700

    initial checkin

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+
+    name="loutools",
+    version="0.0.1",
+    description="Lord of Ultima game tool",
+    author="Troy Scott"
+    author_email="code@troyscott.ca"
+    download_url="https://github.com/troyscott/lou-dash-py/zipball/master"
+    packages=['loutools']
+
+
+)
