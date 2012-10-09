9841dfaa099accf2e2a64f5d4a7a86c5ba0ff51a
Troy Scott <troys2005@gmail.com>
2012-10-08 23:07:01 -0700

    simplified the regex logic in the Session class by using re.search. setup.py file now installs the loutools module.

--- loutools/lousession.py
+++ loutools/lousession.py
@@ -32,39 +32,14 @@ class Session():
         body = dict(mail=mail, password=password)
         response, content = client.request(_URL_LOGIN, 'POST', headers=headers,
                 body=urlencode(body))
-        print 'Response login: %s' % response
+        #print 'Response login: %s' % response
         headers = {'Cookie': response['set-cookie']}
         response, content = client.request(_URL_HIDDEN_SESSION_ID, 'GET',
                 headers=headers)
-        self.session_data = content
-        self.session_id = self.__get_session_id()
-        self.game_url = self.__get_game_url()
-        self.world_id = self.__get_world_id()
+        pattern = re.compile(r'.*id="sessionId" value="(.*)".*')
+        self.session_id = pattern.search(content).groups()[0]
+        pattern = re.compile(r'.*action="(http://prodgame.*/.*/).*".*')
+        self.game_url = pattern.search(content).groups()[0]
+        pattern = re.compile(r'.*value="(World.*)".*')
+        self.world_id = pattern.search(content).groups()[0]
 
-    def __get_session_id(self):
-        find_line = 'name="sessionId" id="sessionId"'
-        re_find_value = r".*name=\"sessionId\" id=\"sessionId\" value=\"(.*)\".*"
-        return self.__parse_session_data(find_line, re_find_value)
-       
-    def __get_game_url(self):
-        find_line = 'action="http://prodgame'
-        re_find_value = r".*action=\"(.*)\/.*\".*"
-        return self.__parse_session_data(find_line, re_find_value)
-    
-    def __get_world_id(self):
-        find_line = 'value="World ' 
-        re_find_value = r".*value=\"(.*)\".*"
-        return self.__parse_session_data(find_line, re_find_value)
-    
-
-    #TODO: replace echo/grep logic with a call to re.search for consistency      
-    def __parse_session_data(self, find_line, re_find_value ):
-        data = self.session_data
-        p1 = Popen(["echo", data], stdout=PIPE)
-        p2 = Popen(["grep", find_line], stdin=p1.stdout, stdout=PIPE)
-        p1.stdout.close()
-        line = p2.communicate()[0]
-        pattern = re.compile(re_find_value)
-        result = pattern.match(line)
-        return result.group(1)
-    
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name="loutools",
     version="0.0.1",
     description="Lord of Ultima game tool",
-    author="Troy Scott"
-    author_email="code@troyscott.ca"
-    download_url="https://github.com/troyscott/lou-dash-py/zipball/master"
-    packages=['loutools']
+    author="Troy Scott",
+    author_email="code@troyscott.ca",
+    download_url="https://github.com/troyscott/lou-dash-py/zipball/master",
+    packages=['loutools'],
 
 
 )
